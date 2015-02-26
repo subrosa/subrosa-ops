84512079904fb017b46660e895090036a27006a5
Josiah Gore <josiahgore@gmail.com>
2015-02-26 12:44:33 -0500

    updates jenkins for java 8 and adds nexus cert

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -u root -p 8080:8080 -v /vagrant/jenkins/jenkins_home:/var/jenkins_home --name jenkins subrosa/jenkins
+sudo docker run --rm -u root -p 8080:8080 -v /vagrant/jenkins/jenkins_home:/jenkins --name jenkins subrosa/jenkins8
